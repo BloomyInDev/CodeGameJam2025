e2c278bf4c4888318690ea7b15fe01b3cdf24f09
mialonl <leo.mialon@etu.umontpellier.fr>
2026-01-22 12:34:27 +0100

    Refonte de Hitbox

--- core/src/main/java/fr/bastienluben/cgj2025/lib/entities/Entity.java
+++ core/src/main/java/fr/bastienluben/cgj2025/lib/entities/Entity.java
@@ -2,12 +2,12 @@ package fr.bastienluben.cgj2025.lib.entities;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Entity implements ISpriteDrawable {
     protected Sprite sprite;
     protected Vector2 position;
-    protected Move mouvement = null;
 
     public Entity(Sprite sprite, Vector2 position) {
         this.sprite = sprite;
@@ -26,13 +26,31 @@ public class Entity implements ISpriteDrawable {
         return position;
     }
 
+    // Raccourcis
+    public float getX(){
+        return position.x;
+    }
+
+    public float getY(){
+        return position.y;
+    }
+
+    public float getWidth(){
+        return sprite.getWidth();
+    }
+
+    public float getHeight(){
+        return sprite.getHeight();
+    }
+
+    public Rectangle getRectangle()
+    {
+        return new Rectangle(position.x, position.y, sprite.getWidth(), sprite.getHeight());
+    }
+
     @Override
     public void draw(SpriteBatch spriteDrawer) {
         spriteDrawer.draw(sprite, position.x, position.y);
-    }
-
-    public void setVitesse(Vector2 vitesse){
-        mouvement = new Move(vitesse);
     }
 
 }
--- core/src/main/java/fr/bastienluben/cgj2025/lib/entities/Hitbox.java
+++ core/src/main/java/fr/bastienluben/cgj2025/lib/entities/Hitbox.java
@@ -4,27 +4,18 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Hitbox extends Entity{
+public class Hitbox {
     private boolean solid; // Si la hitbox doit être solide (comme un mur ou un sol)
     private Rectangle box;
+    private Entity parent;
 
-    public Hitbox(Sprite sprite, Vector2 position, boolean solid){
-        super(sprite, position);
+    public Hitbox(Entity entite) {
+        this.box = entite.getRectangle();
+    }
+
+    public Hitbox(Entity entite, boolean solid){
+        this(entite);
         this.solid = solid;
-    }
-
-    public Hitbox(Sprite sprite, Vector2 position) {
-        this(sprite, position, false);
-        this.box = new Rectangle();
-    }
-
-    public Hitbox(Sprite sprite, Vector2 position, Vector2 taille, boolean solid){
-        this(sprite, position, solid);
-        this.box = new Rectangle(position.x, position.y, taille.x, taille.y);
-    }
-
-    public Hitbox(Sprite sprite, Vector2 position, Vector2 taille){
-        this(sprite, position, taille, false);
     }
 
     public boolean ilToucheLaHitbox(Hitbox hitbox){
