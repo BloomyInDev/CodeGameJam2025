79e864030cfdc470387ce29c555c233aa30c28c0
Romain Therond <romain.therond@etu.umontpellier.fr>
2026-01-23 10:08:03 +0100

    update boss

--- core/src/main/java/fr/bastienluben/cgj2025/screens/BossTest/BosstestScreen.java
+++ core/src/main/java/fr/bastienluben/cgj2025/screens/BossTest/BosstestScreen.java
@@ -1,6 +1,7 @@
 package fr.bastienluben.cgj2025.screens.BossTest;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import fr.bastienluben.cgj2025.Main;
 import fr.bastienluben.cgj2025.lib.AssetManager;
 import fr.bastienluben.cgj2025.screens.AbstractScreen;
@@ -9,6 +10,16 @@ public class BosstestScreen extends AbstractScreen
 {
     public BosstestScreen(Main game, AssetManager assets) {
         super(game, assets);
+    }
+
+    MembreArticulable membre;
+
+
+    @Override
+    public void onLoad(AssetManager assets)
+    {
+        membre = new MembreArticulable(new Vector2(300, 300), new Vector2(0.5f, 0.5f),
+            assets.getTexture("Silksong.jpg"));
     }
 
     @Override
@@ -20,12 +31,14 @@ public class BosstestScreen extends AbstractScreen
     @Override
     public void update(float dt)
     {
-
+        membre.addRotation(180f * dt);
     }
 
     @Override
     public void draw(SpriteBatch batch)
     {
-
+        batch.begin();
+        membre.draw(batch);
+        batch.end();
     }
 }
--- core/src/main/java/fr/bastienluben/cgj2025/screens/BossTest/MembreArticulable.java
+++ core/src/main/java/fr/bastienluben/cgj2025/screens/BossTest/MembreArticulable.java
@@ -5,11 +5,39 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import fr.bastienluben.cgj2025.lib.entities.Element;
 
-import java.util.ArrayList;
 
 public class MembreArticulable extends Element
 {
-    private ArrayList<Vector2> connectorPoints;
+    private static void applyRotation(
+        Vector2 origin,
+        Vector2 output,
+        float degree)
+    {
+        float dx = output.x - origin.x;
+        float dy = output.y - origin.y;
+        float dist = Vector2.dst(output.x, output.y, origin.x, origin.y);
+
+        float rot;
+        if (dx == 0)
+        {
+            rot = dy > 0 ? 90f : -90f;
+        }
+        else
+        {
+            rot = (float)Math.atan2(dy, dx);
+
+            if (dx > 0)
+            {
+                rot += 180f;
+            }
+        }
+
+        output.x = (float)(Math.cos(Math.toRadians(rot + degree)) * dist);
+        output.y = (float)(Math.sin(Math.toRadians(rot + degree)) * dist);
+    }
+
+    private final Vector2[] connectionsData;
+    private final Vector2 origin;
 
     public MembreArticulable(Vector2 positionRef, Vector2 origin, Texture tex)
     {
