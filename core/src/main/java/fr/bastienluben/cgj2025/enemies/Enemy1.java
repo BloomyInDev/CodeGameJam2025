636441712a0d7c0c6bc1bfd3813f415309711901
Romain Therond <romain.therond@etu.umontpellier.fr>
2026-01-23 15:41:51 +0100

    enemies qui spawns

--- core/src/main/java/fr/bastienluben/cgj2025/enemies/Enemy1.java
+++ core/src/main/java/fr/bastienluben/cgj2025/enemies/Enemy1.java
@@ -3,6 +3,7 @@ package fr.bastienluben.cgj2025.enemies;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
 import fr.bastienluben.cgj2025.lib.entities.ISpriteDrawable;
 import fr.bastienluben.cgj2025.lib.ui.Bounds;
 import fr.bastienluben.cgj2025.lib.ui.Button;
@@ -14,18 +15,20 @@ public class Enemy1 implements ISpriteDrawable
     Button but;
     Runnable run;
 
-    public Enemy1(Runnable run, Random rnd, Texture tex)
+    public Enemy1(Runnable run, Random rnd, Texture tex, int indexInArr)
     {
         this.run = run;
         but = new Button(() -> onDie(), tex);
         but.setPosition(Bounds.Center);
-        but.posOffset = new Vector2(rnd.nextInt(360), rnd.nextInt(360));
+        but.posOffset = new Vector2(rnd.nextInt(360) - 180, rnd.nextInt(360) - 180);
     }
+
+
 
     private void onDie()
     {
         run.run();
-        but.dispose();
+        but.hide(true);
     }
 
     public void draw(SpriteBatch batch)
