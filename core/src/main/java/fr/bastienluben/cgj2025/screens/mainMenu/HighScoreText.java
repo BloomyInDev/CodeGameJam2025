df8c15316069be914066d8d441d43789f1ad5e34
Bloomy <contact@bloomyindev.me>
2026-01-24 06:42:51 +0100

    On enleve du debug

--- core/src/main/java/fr/bastienluben/cgj2025/screens/mainMenu/HighScoreText.java
+++ core/src/main/java/fr/bastienluben/cgj2025/screens/mainMenu/HighScoreText.java
@@ -16,8 +16,7 @@ import java.util.Arrays;
 
 public class HighScoreText implements IScript {
     private int highScore;
-    private Text highScoreText, testText;
-    private float timer;
+    private Text highScoreText;
 
     @Override
     public void onLoad(AssetManager manager) {
@@ -36,36 +35,15 @@ public class HighScoreText implements IScript {
 
         // Position AFTER all properties are set
         highScoreText.setPosition(Bounds.TopRight);
-
-        testText = new Text("bottom text");
-        testText.setMargin(10);
-        // Position AFTER all properties are set
-        testText.setPosition(Bounds.Bottom);
-        testText.setFont(smallFont);
     }
 
     @Override
     public void update(float delta) {
-        System.out.println(timer);
-        timer += delta;
-        if (timer > .5) {
-            timer = 0;
-            switch (testText.getPosition()) {
-                case TopRight: testText.setPosition(Bounds.Top); break;
-                case Top: testText.setPosition(Bounds.TopLeft); break;
-                case TopLeft: testText.setPosition(Bounds.Left); break;
-                case Left: testText.setPosition(Bounds.BottomLeft); break;
-                case BottomLeft: testText.setPosition(Bounds.Bottom); break;
-                case Bottom: testText.setPosition(Bounds.BottomRight); break;
-                case BottomRight: testText.setPosition(Bounds.Right); break;
-                case Right: testText.setPosition(Bounds.TopRight); break;
-            }
-        };
     }
 
     @Override
     public void draw(SpriteBatch batch) {
-        Arrays.asList(highScoreText, testText).forEach(e -> e.draw(batch));
+        Arrays.asList(highScoreText).forEach(e -> e.draw(batch));
     }
 
     @Override
